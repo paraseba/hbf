f8036548331d18a0ae8d846411c30e3ee6958ab2
Sebastian Galkin <paraseba@gmail.com>
2018-06-08 00:25:14 -0300

    Trying cabal in gitlab CI

--- release.nix
+++ release.nix
@@ -9,6 +9,8 @@ let
             overrides = haskellPackagesNew: haskellPackagesOld: rec {
               hbf =
                 haskellPackagesNew.callPackage ./default.nix { };
+
+              withCabal = pkgs.haskell.lib.overrideCabal hbf (oldDerivation: {testToolDepends = [pkgs.cabal-install];});
             };
           };
         };
@@ -19,5 +21,7 @@ let
   pkgs = import <nixpkgs> { inherit config; };
 
 in
-  { hbf = pkgs.haskell.packages.${compiler}.hbf;
+  {
+    hbf = pkgs.haskell.packages.${compiler}.hbf;
+    withCabal = pkgs.haskell.packages.${compiler}.withCabal.env;
   }
