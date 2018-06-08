8eefc77aa01a50f5f69d3722e4aaf0a771a17347
Sebastian Galkin <paraseba@gmail.com>
2018-06-08 16:05:19 -0300

    Added basic evaluation test
    Using factor.bf

---
+++ tests/EvalTest.hs
@@ -0,0 +1,18 @@
+module EvalTest where
+
+import qualified Data.Text.Lazy.IO as TIO
+import           Test.HUnit
+import           Control.Monad.Trans.State (execStateT)
+
+import qualified HBF.Compiler      as C
+import qualified HBF.Eval          as E
+import           HBF.Types
+
+unit_canFactorNumbers :: Assertion
+unit_canFactorNumbers = do
+  code <- TIO.readFile "tests/factor.bf"
+  let (Right program) = C.inMemoryCompile C.defaultCompilerOptions code
+  finalState <- exec program
+  mockOutputS finalState @?= "25454: 2 11 13 89\n"
+  where
+    exec program = execStateT (E.eval program) (mkMockIOS "25454\n")
